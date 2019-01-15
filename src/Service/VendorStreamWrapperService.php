fc8d242ce68df82f6831790632e4f40e65605796
martin107 <martin107@2659379.no-reply.drupal.org>
2019-01-15 16:49:09 +1100

    Issue #3025552 by martin107: Add initial test for creatUrlFromUri()

--- src/Service/VendorStreamWrapperService.php
+++ src/Service/VendorStreamWrapperService.php
@@ -2,17 +2,33 @@
 
 namespace Drupal\vendor_stream_wrapper\Service;
 
+use Drupal\Core\StreamWrapper\StreamWrapperManagerInterface;
+
 /**
  * Provides services for the Vendor Stream Wrapper module.
  */
 class VendorStreamWrapperService implements VendorStreamWrapperServiceInterface {
 
   /**
+   * The Stream Wrapper Service.
+   *
+   * @var Drupal\Core\StreamWrapper\StreamWrapperManagerInterface
+   */
+  protected $streamWrapperService;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(StreamWrapperManagerInterface $streamWrapperManager) {
+    $this->streamWrapperService = $streamWrapperManager;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function creatUrlFromUri($uri) {
     if (strpos($uri, 'vendor://') === 0) {
-      if ($wrapper = \Drupal::service('stream_wrapper_manager')->getViaUri($uri)) {
+      if ($wrapper = $this->streamWrapperService->getViaUri($uri)) {
         return $wrapper->getExternalUrl();
       }
     }
