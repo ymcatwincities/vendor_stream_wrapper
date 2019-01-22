420c9cd3d63cd9d73592769847ca30505378dd4b
osopolar <osopolar@115856.no-reply.drupal.org>
2019-01-22 13:53:12 +1100

    Issue #3026190 by osopolar, jaypan: Use MimeTypeGuesser instead of katzien/php-mime-type

--- src/Controller/VendorFileDownloadController.php
+++ src/Controller/VendorFileDownloadController.php
@@ -3,7 +3,9 @@
 namespace Drupal\vendor_stream_wrapper\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use MimeType\MimeType;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesserInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -17,6 +19,46 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 class VendorFileDownloadController extends ControllerBase implements VendorFileDownloadControllerInterface {
 
   /**
+   * The MIME type guesser.
+   *
+   * @var \Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesserInterface
+   */
+  protected $mimeTypeGuesser;
+
+  /**
+   * The log service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * Creates a new VendorFileDownloadController instance.
+   *
+   * @param \Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesserInterface $mimeTypeGuesser
+   *   The MIME type guesser.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerFactory
+   *   The logger factory.
+   */
+  public function __construct(
+    MimeTypeGuesserInterface $mimeTypeGuesser,
+    LoggerChannelFactoryInterface $loggerFactory
+  ) {
+    $this->mimeTypeGuesser = $mimeTypeGuesser;
+    $this->logger = $loggerFactory->get('Vendor File Download'):
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('file.mime_type.guesser'),
+      $container->get('logger.factory')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function download(Request $request) {
@@ -24,11 +66,20 @@ class VendorFileDownloadController extends ControllerBase implements VendorFileD
     $scheme = 'vendor';
     $uri = $scheme . '://' . $filepath;
 
-    if (file_exists($uri)) {
+    $mime_type = '';
+    try {
+      $mime_type = $this->mimeTypeGuesser->guess($uri);
+    }
+    catch (\Exception $e) {
+      $this->logger->error('Vendor file download error: %message', ['%message' => $exception->getMessage()]);
+    }
+
+    if (!empty($mime_type)) {
       $headers = [
-        'Content-Type' => MimeType::getType($uri),
+        'Content-Type' => $mime_type,
       ];
-      return new BinaryFileResponse($uri, 200, [], TRUE);
+
+      return new BinaryFileResponse($uri, 200, $headers, TRUE);
     }
 
     throw new NotFoundHttpException();
